85f5214fbc4418aa44c6280cdaeeebfa5bcd45b2
kaustubhraut2001 <kaustubhr2001@gmail.com>
2025-04-09 20:54:26 +0530

    Some fixes

--- client/src/pages/RoomPage.jsx
+++ client/src/pages/RoomPage.jsx
@@ -1,77 +1,89 @@
-import React, { useEffect , useCallback, useState } from 'react'
-import { useSocket } from '../providers/Socket'
-import { usePeer} from '../providers/Peer';
-import ReactPlayer from 'react-player' 
-
+import React, { useEffect, useCallback, useState } from "react";
+import { useSocket } from "../providers/Socket";
+import { usePeer } from "../providers/Peer";
+import ReactPlayer from "react-player";
 
 const RoomPage = () => {
-  const {socket} = useSocket();
-  const {peer , createOffer , createAnswer , setRemoteAnswer,sendStream,remoteStream} = usePeer();
-  console.log(createOffer ,"create offer");
+  const { socket } = useSocket();
+  const {
+    peer,
+    createOffer,
+    createAnswer,
+    setRemoteAnswer,
+    sendStream,
+    remoteStream,
+  } = usePeer();
+  console.log(createOffer, "create offer");
 
   const [myStream, setMyStream] = useState(null);
   // const [remoteStream, setRemoteStream] = useState(null);
 
-  const handelNewUserJoined = useCallback(async(data) => {
-    // console.log("User joined room", emailId);
-    const {emailId} = data;
-    const offer = await createOffer();
+  const handelNewUserJoined = useCallback(
+    async (data) => {
+      // console.log("User joined room", emailId);
+      const { emailId } = data;
+      const offer = await createOffer();
 
-    socket.emit("call-user", {
-      emailId,
-      offer
-    });
-    
-  } , [socket, createOffer]);
+      socket.emit("call-user", {
+        emailId,
+        offer,
+      });
+    },
+    [socket, createOffer]
+  );
 
-  const handleIncomingCall = useCallback(async(data) => {
-    console.log("Incoming call", data);
-    const {from , offer} = data;
-    const ans = await createAnswer(offer);
-    socket.emit("call-accepted", {emailId : from, ans});
-  }, [socket, createAnswer]);
+  const handleIncomingCall = useCallback(
+    async (data) => {
+      console.log("Incoming call", data);
+      const { from, offer } = data;
+      const ans = await createAnswer(offer);
+      socket.emit("call-accepted", { emailId: from, ans });
+    },
+    [socket, createAnswer]
+  );
 
-  const handleCallAccepted = useCallback(async(data) => {
-    console.log("Call accepted", data);
-    const {ans} = data;
-    await setRemoteAnswer(ans);
-    await sendStream(myStream);
-    console.log("Remote answer set", ans);
-
-  }, [socket, createAnswer]);
+  const handleCallAccepted = useCallback(
+    async (data) => {
+      console.log("Call accepted", data);
+      const { ans } = data;
+      await setRemoteAnswer(ans);
+      await sendStream(myStream);
+      console.log("Remote answer set", ans);
+    },
+    [socket, createAnswer]
+  );
 
   useEffect(() => {
     socket.on("user-joined", handelNewUserJoined);
     socket.on("incoming-call", handleIncomingCall);
     socket.on("call-accepted", handleCallAccepted);
 
-  
-
-
     return () => {
       socket.off("user-joined", handelNewUserJoined);
       socket.off("incoming-call", handleIncomingCall);
       socket.off("call-accepted", handleCallAccepted);
-    }
+    };
   }, [socket, handelNewUserJoined, handleIncomingCall]);
 
+  const getUserMediaStream = useCallback(async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+
+    setMyStream(stream);
+  }, []);
   useEffect(() => {
     getUserMediaStream();
-  },[getUserMediaStream]);
-
-  const getUserMediaStream = useCallback(async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({video : true , audio : true});
-   
-    setMyStream(stream);
-  } , []); 
+  }, [getUserMediaStream]);
 
   return (
     <div>
       room page
-      <ReactPlayer stream={myStream} playing muted={true} />
-      <ReactPlayer stream={remoteStream} playing  />
+      <ReactPlayer stream={myStream} playing={true} muted={true} />
+      <ReactPlayer stream={remoteStream} playing={true} muted={true} />
     </div>
-  )
-}
+  );
+};
 
-export default RoomPage
+export default RoomPage;
--- client/src/providers/Peer.jsx
+++ client/src/providers/Peer.jsx
@@ -1,71 +1,87 @@
-import { useMemo, createContext, useContext, useEffect, useCallback } from "react";
-
+import {
+  useMemo,
+  createContext,
+  useContext,
+  useEffect,
+  useCallback,
+  useState,
+} from "react";
 
 const PeerContext = createContext();
 
 export const usePeer = () => {
-    return useContext(PeerContext);
+  return useContext(PeerContext);
 };
 
 export const PeerProvider = ({ children }) => {
+  const [remoteStream, setRemoteStream] = useState(null);
 
-    const [remoteStream, setRemoteStream] = useState(null);
-
-    const peer = useMemo(() => new RTCPeerConnection(
-        {
-            iceServers: [
-                {
-                    urls: ["stun:stun1.l.google.com:19302",
-                        "stun:global.stun.twilio.com:3478",
-                    ]
-                },
+  const peer = useMemo(
+    () =>
+      new RTCPeerConnection({
+        iceServers: [
+          {
+            urls: [
+              "stun:stun1.l.google.com:19302",
+              "stun:global.stun.twilio.com:3478",
             ],
-        }
-    ), []);
+          },
+        ],
+      }),
+    []
+  );
 
-    const createOffer = async () => {
-        const offer = await peer.createOffer();
+  const createOffer = async () => {
+    const offer = await peer.createOffer();
 
-        console.log(offer, "offer");
+    console.log(offer, "offer");
 
+    await peer.setLocalDescription(offer);
+    return offer;
+  };
+  const createAnswer = async (offer) => {
+    await peer.setRemoteDescription(offer);
+    const answer = await peer.createAnswer();
+    await peer.setLocalDescription(answer);
+    return answer;
+  };
 
-        await peer.setLocalDescription(offer);
-        return offer;
+  const setRemoteAnswer = async (answer) => {
+    await peer.setRemoteDescription(answer);
+  };
+
+  const sendStream = async (stream) => {
+    const Tracks = stream.getTracks();
+    Tracks.forEach((track) => {
+      peer.addTrack(track, stream);
+    });
+  };
+
+  const handleTrackEvent = useCallback((event) => {
+    const stream = event.streams[0];
+    setRemoteStream(stream);
+  }, []);
+
+  useEffect(() => {
+    peer.addEventListener("track", handleTrackEvent);
+
+    return () => {
+      peer.removeEventListener("track", handleTrackEvent);
     };
-    const createAnswer = async (offer) => {
-        await peer.setRemoteDescription(offer);
-        const answer = await peer.createAnswer();
-        await peer.setLocalDescription(answer);
-        return answer;
-    };
+  }, [peer, handleTrackEvent]);
 
-    const setRemoteAnswer = async (answer) => {
-        await peer.setRemoteDescription(answer);
-    };
-
-    const sendStream = async (stream) => {
-        const Tracks = stream.getTracks();
-        Tracks.forEach((track) => {
-            peer.addTrack(track, stream);
-        });
-    };
-
-    const handleTrackEvent = useCallback((event) => {
-        const stream = event.streams[0];
-        setRemoteStream(stream);
-    }, []);
-
-    useEffect(() => {
-        peer.addEventListener("track", handleTrackEvent);
-
-        return () => {
-            peer.removeEventListener("track", handleTrackEvent);
-        };
-    }, [peer, handleTrackEvent]);
-
-    return(
-        <PeerContext.Provider value={{peer, createOffer,createAnswer,setRemoteAnswer,sendStream,remoteStream}}>
-            {children}
-        </PeerContext.Provider>
-    )
+  return (
+    <PeerContext.Provider
+      value={{
+        peer,
+        createOffer,
+        createAnswer,
+        setRemoteAnswer,
+        sendStream,
+        remoteStream,
+      }}
+    >
+      {children}
+    </PeerContext.Provider>
+  );
 };
