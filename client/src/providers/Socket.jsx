2a0e60c2fb1cb66fe92f1659ce3e11d547f07044
kaustubhraut2001 <kaustubhr2001@gmail.com>
2025-04-09 22:12:17 +0530

    added production url and tesetd

--- client/src/providers/Socket.jsx
+++ client/src/providers/Socket.jsx
@@ -1,25 +1,23 @@
-import { createContext, useContext, useMemo } from 'react';
-import {io }from 'socket.io-client';
+import { createContext, useContext, useMemo } from "react";
+import { io } from "socket.io-client";
 
 const socketContext = createContext(null);
 
-
 export const useSocket = () => {
-    return useContext(socketContext);
+  return useContext(socketContext);
 };
 
 export const SocketProvider = ({ children }) => {
-    // const socket = useMemo(() => {
-    //     io("http://localhost:8001")
-    //     },
-    //     []);
+  // const socket = useMemo(() => {
+  //     io("http://localhost:8001")
+  //     },
+  //     []);
 
-    const socket = useMemo(() => io("http://localhost:8001"), []);
+  const socket = useMemo(() => io("http://localhost:8001"), []);
 
-
-    return (
-        <socketContext.Provider value={{socket}}>
-            {children}
-        </socketContext.Provider>
-    );
+  return (
+    <socketContext.Provider value={{ socket }}>
+      {children}
+    </socketContext.Provider>
+  );
 };
